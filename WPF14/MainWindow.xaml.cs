da8a917bea2b973b4d607642786a4dd4abce33f4
milov <milov@DESKTOP-M98P6EU>
2022-01-24 18:22:28 +0300

    Добавьте файлы проекта.

---
+++ WPF14/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace WPF14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product("Молоко", 80, "ProductsPic/Молоко.png", category.Еда));
+            products.Add(new Product("Йогурт", 60, "ProductsPic/Йогурт.jpg", category.Еда));
+            products.Add(new Product("Сливки", 120, "ProductsPic/Сливки.jpg", category.Еда));
+            products.Add(new Product("Блендр", 4000, "ProductsPic/Блендр.jpg", category.Бытовая_техника));
+            products.Add(new Product("СВЧ-печь", 7000, "ProductsPic/СВЧ-печь.jpg", category.Бытовая_техника));
+            ProductList.ItemsSource = products;
+        }
+    }
+}
---
+++ WPF14/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF14
+{
+    public enum category
+    {
+        Еда,
+        Бытовая_техника
+    }
+    public class Product
+    {
+        public string productName { get; set; }
+        public int productCost { get; set; }
+        public string productPicture { get; set; }        
+        public category productCategory { get; set; }       
+        public Product(string Name, int Cost, string Picture, category Category)
+        {
+            this.productName = Name;
+            this.productCost = Cost;
+            this.productPicture = Picture;
+            this.productCategory = Category;
+        }
+    }
+}
